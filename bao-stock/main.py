59cdab538deca7a15167f467a76d9dc4029eb6e0
lin <rukbate@hotmail.com>
2021-07-11 19:29:45 +0800

    bt plot

--- bao-stock/main.py
+++ bao-stock/main.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 class TestStrategy(bt.Strategy):
     params = (
-        ('exitbars', 5),
+        ('maperiod', 15),
     )
 
     def log(self, txt, dt=None):
@@ -21,6 +21,19 @@ class TestStrategy(bt.Strategy):
         self.buyprice = None
         self.buycomm = None
 
+        self.sma = bt.indicators.SimpleMovingAverage(
+            self.datas[0], 
+            period=self.params.maperiod
+        )
+
+        bt.indicators.ExponentialMovingAverage(self.datas[0], period=25)
+        bt.indicators.WeightedMovingAverage(self.datas[0], period=25, subplot=True)
+        bt.indicators.StochasticSlow(self.datas[0], safediv=True)
+        bt.indicators.MACDHisto(self.datas[0])
+        rsi = bt.indicators.RSI(self.datas[0])
+        bt.indicators.SmoothedMovingAverage(rsi, period=10)
+        bt.indicators.ATR(self.datas[0], plot=False)
+
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
             return
@@ -31,7 +44,7 @@ class TestStrategy(bt.Strategy):
 
                 self.buyprice = order.executed.price
                 self.buycomm = order.executed.comm
-            elif order.issell():
+            else:
                 self.log(f'SELL EXECUTED, Price: {order.executed.price}, Cost: {order.executed.value}, Comm {order.executed.comm}')
             
             self.bar_executed = len(self)
@@ -48,39 +61,38 @@ class TestStrategy(bt.Strategy):
         self.log(f'OPERATION PROFIT, GROSS {trade.pnl}, NET {trade.pnlcomm}')
 
     def next(self):
-        self.log('Open: %.2f, Close: %.2f' % (self.dataopen[0], self.dataclose[0]))
+        # self.log('Open: %.2f, Close: %.2f' % (self.dataopen[0], self.dataclose[0]))
 
         if self.order:
             return 
 
         if not self.position:
-            if self.dataclose[0] < self.dataclose[-1]:
-                if self.dataclose[-1] < self.dataclose[-2]:
-                # if self.dataclose[-2] < self.dataclose[-3]:
-                    self.log(f'BUY CREATE, {self.dataclose[0]}')
-                    self.order = self.buy()
+            if self.dataclose[0] > self.sma[0]:                
+                self.log(f'BUY CREATE, {self.dataclose[0]}')
+                self.order = self.buy()
         else:
-            if len(self) >= (self.bar_executed + self.params.exitbars):
+            if self.dataclose[0] < self.sma[0]:
                 self.log(f'SELL CREATE, {self.dataclose[0]}')
                 self.order = self.sell()
 
 cerebro = bt.Cerebro()
 cerebro.addstrategy(TestStrategy)
 
-dataframe = dk.getDailyK('sh', '601888')
+dataframe = dk.getDailyK('sh', '601888', '2019-01-01')
 
 data = bt.feeds.PandasData(dataname=dataframe)
 
 cerebro.adddata(data)
 cerebro.broker.setcash(100000.0)
 cerebro.addsizer(bt.sizers.FixedSize, stake=10)
-cerebro.broker.setcommission(commission=0.001)
+cerebro.broker.setcommission(commission=0.0)
 
 print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
 cerebro.run()
 
 print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+cerebro.plot()
 
 # dk.syncExistingStocks()
 # idx.updateAllIndexes()
