538f9316ae7f81cdec33d1c0b1930a47a75e6685
lin <rukbate@hotmail.com>
2021-11-01 18:13:14 +0800

    enable stock sync

--- bao-stock/main.py
+++ bao-stock/main.py
@@ -6,12 +6,12 @@ import teststrategy as ts
 import smastrategy as ss
 import testsignal
 
-testsignal.runTest(dataframe=dk.getDailyK('sh', '601888', '2020-01-01', '2020-12-31'))
+# testsignal.runTest(dataframe=dk.getDailyK('sh', '601888', '2020-01-01', '2020-12-31'))
 # ss.runstrat(dataframe=dk.getDailyK('sh', '601888', '2020-01-01', '2020-12-31'))
 # ts.runTest(dataframe = dk.getDailyK('sh', '601888', '2020-01-01'))
 
 # idx.updateAllIndexes()
-# dk.syncExistingStocks()
+dk.syncExistingStocks()
 
 # idx.updateIndex('sz50')
 # print(info.getInfo('sh', '600240'))
